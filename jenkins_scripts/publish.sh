3132f6236c15ed002dac16a8489a6e4919f8017e
Nanda Kishore KH <nandakishore323@gmail.com>
2023-03-23 15:03:39 +0530

    QL-397: Add poetry to PATH

--- jenkins_scripts/publish.sh
+++ jenkins_scripts/publish.sh
@@ -9,7 +9,7 @@ set -ex
 echo "$BRANCH_NAME"
 
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/install-poetry.py | python -
-export PATH="$HOME/.poetry/bin:$PATH"
+export PATH="/root/.local/bin:$PATH"
 
 poetry lock --no-update
 poetry install --no-root
